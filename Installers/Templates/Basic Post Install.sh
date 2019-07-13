1c0c7c49797d4cb557ba6db9e91d855d84164b7b
Simon <SCTCoding@users.noreply.github.com>
2019-07-13 12:38:33 -0400

    Update and rename Post Install.sh to Basic Post Install.sh

--- Installers/Templates/Basic Post Install.sh
+++ Installers/Templates/Basic Post Install.sh
@@ -4,10 +4,11 @@
 install_dir=$(dirname $0)
 ## Assumes contents are in a folder.
 foldername=""
+pkgname=""
 
 if [[ -d "$install_dir/$foldername" ]]
 then
-  t
+  /usr/sbin/installer -pkg "$install_dir"/"$foldername"/"$pkgname" -target /
 else
   echo "File does not exist. Failing."
   exit 1
