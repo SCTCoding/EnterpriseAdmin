772425c683c07b22af7fd0b76976a00b52ead664
Simon <SCTCoding@users.noreply.github.com>
2019-08-09 08:49:23 -0400

    Update and rename Hacky-Hompage to Hacky-Hompage.sh

--- JAMF/Firefox/Hacky-Hompage.sh
+++ JAMF/Firefox/Hacky-Hompage.sh
@@ -1,17 +1,24 @@
 #! /bin/bash
 
+## Determine logged in user
 loggedInUser=$(stat -f "%Su" /dev/console)
 
+## Determine profile name for current Firefox user
 profileName=$(cat /Users/$loggedInUser/Library/Application\ Support/Firefox/profiles.ini | grep "Default=Profiles" | cut -d '/' -f2)
 
+## Determine directory where profile lives
 profileDir="/Users/$loggedInUser/Library/Application Support/Firefox/Profiles"
 
+## Determine current home page
 homePage=$(cat "$profileDir/$profileName"/prefs.js | grep '"browser.startup.homepage",' | cut -d ',' -f2 | cut -d '"' -f2)
 
+## Find the prefs.js and edit
 if [[ $(cat "$profileDir/$profileName"/prefs.js | grep -c '"browser.startup.homepage",') -eq 1 ]]
 then
+	## If exists edit with sed
 	sed -i '' "s/$homePage/www.jamf.com/g" "$profileDir/$profileName/prefs.js"
 else
+	## If not exist then postpend with echo
 	echo 'user_pref("browser.startup.homepage", "www.jamf.com");' >> "$profileDir/$profileName"/prefs.js
 fi
 
