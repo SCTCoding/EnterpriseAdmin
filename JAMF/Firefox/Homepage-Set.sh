85d6b8a989cc48218cc0cd83387a4e65fbb489b5
Simon <SCTCoding@users.noreply.github.com>
2019-08-09 08:46:03 -0400

    Update Homepage-Set.sh

--- JAMF/Firefox/Homepage-Set.sh
+++ JAMF/Firefox/Homepage-Set.sh
@@ -1,9 +1,13 @@
 #! /bin/bash
 
+## Desired Homepage
 homepage="https://www.jamf.com"
+#homepage="$4"
 
+## Enable enterprise management
 defaults write /Library/Preferences/org.mozilla.firefox EnterprisePoliciesEnabled -bool TRUE
 
+## Set homepage
 defaults write /Library/Preferences/org.mozilla.firefox Homepage -dict-add URL -string "$homepage"
 
 exit 0
