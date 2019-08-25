34da2ee6bbf680145d5f27cb98e5816384a7beb9
Simon <SCTCoding@users.noreply.github.com>
2019-08-25 18:16:37 -0400

    Update Program And Version.sh

--- JAMF/Extension Attributes/Program And Version.sh
+++ JAMF/Extension Attributes/Program And Version.sh
@@ -1,5 +1,11 @@
 #! /bin/bash
 
+##########
+# This extension attribute collects if an application is installed 
+# and reports Installed version or not installed.
+##########
+
+
 ## Global variables
 applicationName=""
 osqueryEvent=""
@@ -11,12 +17,12 @@ then
 fi
 
 ## Check for application
-appCheck=$(/usr/local/bin/osqueryi --header=false --line "select name, bundle_short_version from apps where name is \"$applicationName\"" --separator ' ')
+appCheck=$(/usr/local/bin/osqueryi --header=false --line "select name, bundle_short_version from apps where name is \"$applicationName\"")
 
 ## Primary check logic. Outputs application name and version.
 if [[ ! -z "$appCheck" ]]
 then
-  echo "<result>$appCheck</result>"
+  echo "<result>Installed $(echo $appCheck | cut -d '|' -f2 | tail -n1)</result>"
 else
   echo "<result>Not Installed</result>"
 fi
