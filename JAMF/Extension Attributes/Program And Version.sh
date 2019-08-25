738ff8c49c1bd3f25f8b15932b9bd0780503f4dd
Simon <SCTCoding@users.noreply.github.com>
2019-08-25 18:28:05 -0400

    Update Program And Version.sh

--- JAMF/Extension Attributes/Program And Version.sh
+++ JAMF/Extension Attributes/Program And Version.sh
@@ -17,13 +17,17 @@ then
 fi
 
 ## Check for application
-appCheck=$(/usr/local/bin/osqueryi --header=false --line "select name, bundle_short_version from apps where name is \"$applicationName\"")
+appCheck=$(/usr/local/bin/osqueryi --header=false --list "select name, bundle_short_version from apps where name is \"$applicationName\"")
 
 ## Primary check logic. Outputs application name and version.
 if [[ ! -z "$appCheck" ]]
 then
-  ## Outputs installed and version to avoid the corner case where the bundle version may be blank.
-  echo "<result>Installed $(echo $appCheck | cut -d '|' -f2 | tail -n1)</result>"
+  if [[ ! -z $(echo $appCheck | cut -d '|' -f2 | tail -n1) ]]
+  then
+    echo "<result>$(echo $appCheck | cut -d '|' -f2 | tail -n1)</result>"
+  else
+    echo "<result>Installed</result>"
+  fi
 else
   echo "<result>Not Installed</result>"
 fi
