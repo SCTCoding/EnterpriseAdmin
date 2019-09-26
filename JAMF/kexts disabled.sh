6f6da006dead808940d5c872ddbfc182e129683b
Simon <SCTCoding@users.noreply.github.com>
2019-09-26 12:21:58 -0400

    Add files via upload

---
+++ JAMF/kexts disabled.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+# Find all computers with Kexts that are not approved. Dump to an attribute.
+
+## Determine if the policy file is there.
+if [[ -f '/var/db/SystemPolicyConfiguration/KextPolicy' ]]
+then
+	## Find any disabled kexts.
+	kextsDisabled=$(sqlite3 -list -separator ', ' /var/db/SystemPolicyConfiguration/KextPolicy 'select bundle_id, team_id from kext_policy where allowed = 0;')
+	output=()
+	for kext in "${kextsDisabled[@]}"
+	do
+		output+="$kext "
+	done
+
+	## Evaluate the output
+	if [[ $(echo "$output" | grep -c "," ) -ge 1 ]]
+	then
+		echo "<result>Disabled Kexts: $output</result>"
+	else
+		echo "<result>All Kexts Enabled</result>"
+	fi
+else
+	echo "<result>No KextPolicy Database</result>"
+fi
+
+exit 0
