f4b0182dcdb6bb3c70da61430ebb06d11b7d43b8
Simon <SCTCoding@users.noreply.github.com>
2020-03-22 16:00:02 -0400

    Add files via upload

---
+++ LoggingFunction.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+
+function logToFile {
+	NOW=$(date +%s)
+	NOWH=$(date "+%Y-%m-%d %H:%M:%S")
+	logPath="$1"
+
+	if [[ -f "$logPath" ]] && [[ $(du -h "$logPath" | awk '{print $1}' | grep -c "M") -eq 1 ]] && [[ $(du -h "$logPath" | awk '{print $1}' | grep -oE '^[0-9]{1,4}') -gt 20 ]]
+	then
+		echo "ROTATED: $NOW" > "$logPath"
+		echo "STARTED: $NOW" >> "$logPath"
+		echo "$NOW $NOWH $2" >> "$logPath"
+		return
+	elif [[ -f "$logPath" ]] && [[ $(du -h "$logPath" | awk '{print $1}' | grep -cE "G|T|P|E") -eq 1 ]]
+	then
+		echo "ROTATED: $NOW" > "$logPath"
+		echo "STARTED: $NOW" >> "$logPath"
+		echo "$NOW $NOWH $2" >> "$logPath"
+		return
+	fi
+
+	if [[ -f "$logPath" ]] && [[ $(du -h "$logPath" | awk '{print $1}' | grep -cE 'B|K|M') -eq 1 ]]
+	then
+		echo "$NOW $NOWH $2" >> "$logPath"
+		return
+	else
+		echo "STARTED: $NOW" >> "$logPath"
+		echo "$NOW $NOWH $2" >> "$logPath"
+		return 
+	fi
+}
+
+logToFile 
+
