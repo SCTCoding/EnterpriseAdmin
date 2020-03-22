1a457e1362b922138380f8b8941e83812b7de6e0
Simon <SCTCoding@users.noreply.github.com>
2020-03-22 18:48:24 -0400

    Update LoggingFunction.sh

--- LoggingFunction.sh
+++ LoggingFunction.sh
@@ -4,6 +4,7 @@ function logToFile {
 	NOW=$(date +%s)
 	NOWH=$(date "+%Y-%m-%d %H:%M:%S")
 	logPath="$1"
+	## $2 is the log text output which can be specified as a variable $log_text
 
 	if [[ -f "$logPath" ]] && [[ $(du -h "$logPath" | awk '{print $1}' | grep -c "M") -eq 1 ]] && [[ $(du -h "$logPath" | awk '{print $1}' | grep -oE '^[0-9]{1,4}') -gt 20 ]]
 	then
